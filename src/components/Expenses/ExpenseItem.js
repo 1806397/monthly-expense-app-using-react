3b36ecc048a832c02d86731b7e1223b0c358e405
Piyush Singh <1806397@kiit.ac.in>
2022-10-17 14:58:31 +0530

    minor changes

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,10 +3,6 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 import React, { useState } from "react";
 function ExpenseItem(props) {
-  // const [title, setTitle] = useState(props.title);
-  // const clickHandler = () => {
-  //   setTitle("Updated!");
-  // };
   return (
     <list>
       <Card className="expense-item">
@@ -15,7 +11,6 @@ function ExpenseItem(props) {
           <h2>{props.title}</h2>
           <div className="expense-item__price">${props.amount}</div>
         </div>
-        <button>Change Title</button>
       </Card>
     </list>
   );
